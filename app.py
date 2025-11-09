81ab316a9cbb5599eb8b05f86c793e3e5b3335a9
aryan7786engg-sudo <aryan7786engg@gmail.com>
2025-11-09 15:33:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from googleapiclient.discovery import build
 # ----------------------------
 # ✅ CONFIG
 # ----------------------------
-YOUTUBE_API_KEY = "AIza......your real key here..."  # <-- PUT YOUR YOUTUBE API KEY HERE
+YOUTUBE_API_KEY = "AIzaSyCjIO1SrRPZ1BnevEgvkfUsEj4uIS2VCE4"  # <-- PUT YOUR YOUTUBE API KEY HERE
 
 st.set_page_config(page_title="Alzheimer Detection", layout="centered")
 
